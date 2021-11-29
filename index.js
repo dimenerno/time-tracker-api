e788d6d1f1faf29ace33bcdb453d075de5deea4e
dimen <dimenerno@gmail.com>
2021-11-29 20:12:33 +0900

    Change to use database

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 const express = require('express')
 const cors = require("cors")
 const bodyParser = require('body-parser')
+const RecordModel = require('./model')
+const mongoose = require('mongoose')
+const db = require('./db')
 
 const app = express()
 const port = 9000
+
+mongoose.connect('mongodb://localhost:27017', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+})
+
+const database = mongoose.connection
+database.once('open', function () {
+   console.log('DB connected!')
+})
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const report = {
-   Study: 0,
-   Read: 0,
-   Work: 0,
-   Exercise: 0,
-   Leisure: 0
-}
-
 app.post('/', (req, res) => {
-   const json = req.body
-   report[json.category] += json.time
-   console.log(report)
-   res.send(report)
+   const new_json = req.body
+   db.update(new_json.category, new_json.time, () => res.send('ok'))
 })
 
 app.get('/', (req, res) => {
-   res.send({report: report})
+   db.getToday(today_json => res.send(today_json))
 })
 
 app.listen(port, () => {
